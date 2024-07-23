9b4ac46c225b0ea8f8da2a965c7ff0ee09335bfb
admin <khadijaaws21@gmail.com>
2024-07-23 12:20:06 +0200

    first

---
+++ husky.config.js
@@ -0,0 +1,6 @@
+// husky.config.js
+module.exports = {
+    hooks: {
+      'commit-msg': 'test "$(git log -1 --pretty=%B)" != "fix: correct code style issues with ESLint" && commitlint -E HUSKY_GIT_PARAMS || true',
+    },
+  };
