b63e2239b8292d001de2c0b130a738bc330bad71
Lint Action <lint-action@samuelmeuli.com>
2024-07-23 10:37:49 +0000

    Fix code style issues with ESLint

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
 ;(function (exports) {
   function greet(name) {
     const options = {
-
-
-
-
-      
       minute: 'numeric',
       hour: 'numeric',
       day: 'long',
-      week: "numeric",
+      week: 'numeric',
       month: 'long',
       year: 'numeric',
-      weekday: "nymeric",
+      weekday: 'nymeric',
     }
 
-    let now = new Date();
-    const formattedDate = now.toLocaleDateString('en-US', options);
+    const now = new Date()
+    const formattedDate = now.toLocaleDateString('en-US', options)
     return `Hello, ${name}! Today is ${formattedDate}.`
   }
 
-  exports.greet = greet;
+  exports.greet = greet
 })(this)
