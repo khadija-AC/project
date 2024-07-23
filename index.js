153a506c4b5c6d9bfbefbeff8aa9cc432cfc544b
admin <khadijaaws21@gmail.com>
2024-07-23 11:34:39 +0200

    first commit

--- index.js
+++ index.js
@@ -8,10 +8,10 @@
       week: "numeric",
       month: 'long',
       year: 'numeric',
-      weekday: 'nymeric',
+      weekday: "nymeric",
     }
 
-    const now = new Date();
+    let now = new Date();
     const formattedDate = now.toLocaleDateString('en-US', options);
     return `Hello, ${name}! Today is ${formattedDate}.`
   }
