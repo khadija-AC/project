e138200e1a3542a024da6599d966f8556399dae4
admin <khadijaaws21@gmail.com>
2024-07-23 15:11:19 +0200

    commit

--- index.js
+++ index.js
@@ -16,7 +16,7 @@
     }
     
     const a=0;
-    const now = new Date()
+    let now = new Date()
     const formattedDate = now.toLocaleDateString('en-US', options)
     return `Hello, ${name}! Today is ${formattedDate}.`
   }
