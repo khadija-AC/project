1ce2d995f1e43ddd49a2d55f2b804805f0355d8b
admin <khadijaaws21@gmail.com>
2024-07-30 16:13:13 +0200

    chore: update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 ;(function (exports) {
   function greet(name) {
     const options = {
-      microseconds: 'numeric',
+     
       second: 'numeric',
       minute: 'numeric',
       hour: 'numeric',
