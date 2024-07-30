87051f688a258d737a76cdec320355428f104a2d
admin <khadijaaws21@gmail.com>
2024-07-30 16:29:54 +0200

    chore: update index.js

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 ;(function (exports) {
   function greet(name) {
     const options = {
-     
       second: 'numeric',
       minute: 'numeric',
       hour: 'numeric',
       day: 'long',
       week: 'numeric',
       month: 'long',
-      year: "numeric",
-     
+      year: 'numeric',
     }
-
+    const a=0;
     const now = new Date()
     const formattedDate = now.toLocaleDateString('en-US', options)
     return `Hello, ${name}! Today is ${formattedDate}.`
