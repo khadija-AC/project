38c65abe12e2434245c2c7586a6556207b5a0dff
admin <khadijaaws21@gmail.com>
2024-08-01 15:04:11 +0200

    feat(hi): every one

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 ;(function (exports) {
   function greet(name) {
     const options = {
-
-
-
-
+      microsecond: 'numeric',
       second: 'numeric',
       minute: 'numeric',
       hour: 'numeric',
       day: 'long',
       week: 'numeric',
       month: 'long',
-      year: "numeric",
+      year: 'numeric',
     }
-   
-    let now = new Date()
+
+    const now = new Date()
     const formattedDate = now.toLocaleDateString('en-US', options)
     return `Hello, ${name}! Today is ${formattedDate}.`
   }
